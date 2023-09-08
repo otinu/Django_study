8d15e0c4f80d08eb7c467a0267181af570deae78
otinu <fitshter@gmail.com>
2023-09-09 03:27:48 +0900

    CreateViewのサンプルを追加しました

--- todoProject/todo/views.py
+++ todoProject/todo/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from .models import TodoModel
+
+from django.urls import reverse_lazy
 
 class TodoList(ListView):
     template_name = 'list.html'
@@ -9,3 +11,9 @@ class TodoList(ListView):
 class TodoDetail(DetailView):
     template_name = 'detail.html'
     model = TodoModel
+
+class TodoCreate(CreateView):
+    template_name = 'create.html'
+    model = TodoModel
+    fields = ('title', 'memo', 'priority', 'duedate') # form.〇〇でどのフィールドを使うのか指定する
+    success_url = reverse_lazy('list') # urls.pyの別名(path()の第三引数)を指定
