47aed2eca897a126b7ba160d8b60c28ce24aee9d
otinu <fitshter@gmail.com>
2023-09-09 08:13:25 +0900

    ユーザー登録機能(signup)サンプルを追加しました

--- boardproject/boardapp/views.py
+++ boardproject/boardapp/views.py
@@ -1,16 +1,27 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from django.db import IntegrityError
 
 def signupfunc(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
 
-    # Modelデータ取得サンプル
+        try:
+            # この行が実行される時点でユーザー登録は完了する
+            user = User.objects.create_user(username, '', password)
+        except IntegrityError:
+            return render(request, 'signup.html', {'error_message': 'ユーザーは登録済みでした'})
+
+        # この行で登録内容の更新などが実行される
+        # user.save()
+
+    '''
+    Modelデータ取得サンプル
+
     # my_object = User.objects.all()
     my_object = User.objects.get(username='tester')
     # print(my_object)
     print(my_object.email)
-
-    if request.method == "POST":
-        print("Hello")
-    else:
-        print("No")
+    '''
     return render(request, 'signup.html', {'some': 100})
