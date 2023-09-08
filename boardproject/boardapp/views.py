eaa0d66f30acf261d5fd3109756efe376487fc9d
otinu <fitshter@gmail.com>
2023-09-09 06:54:34 +0900

    Modelデータ取得サンプルを追加しました

--- boardproject/boardapp/views.py
+++ boardproject/boardapp/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 def signupfunc(request):
+
+    # Modelデータ取得サンプル
+    # my_object = User.objects.all()
+    my_object = User.objects.get(username='tester')
+    # print(my_object)
+    print(my_object.email)
+
+    if request.method == "POST":
+        print("Hello")
+    else:
+        print("No")
     return render(request, 'signup.html', {'some': 100})
