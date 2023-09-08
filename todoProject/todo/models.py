0b8a176ae0911a3a7db63256330392cbd1f72f2e
otinu <fitshter@gmail.com>
2023-09-08 23:33:00 +0900

    save

--- todoProject/todo/models.py
+++ todoProject/todo/models.py
@@ -3,3 +3,11 @@ from django.db import models
 class TodoModel(models.Model):
     title = models.CharField(max_length=100)
     memo = models.TextField()
+    priority = models.CharField(
+        max_length = 50,
+        choices = CHOICE
+    )
+    duedate = models.DateField()
+
+    def __str__(self):
+        return slf.title
